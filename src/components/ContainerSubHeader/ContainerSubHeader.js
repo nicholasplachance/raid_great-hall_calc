c0f4c51e6b6e7833b76c6acd21a6927c20840411
Nicholas LaChance <nicholasplachance@gmail.com>
2021-05-25 15:29:44 -0500

    Update UI for responsive design, add colors to affinity names

--- src/components/ContainerSubHeader/ContainerSubHeader.js
+++ src/components/ContainerSubHeader/ContainerSubHeader.js
@@ -3,7 +3,7 @@ import './ContainerSubHeader.css';
 const ContainerSubHeader = (props) => {
 	return (
 		<div className='container-subheader'>
-			<div className='spacer'></div>
+			<div className='spacer stats'>Affinity</div>
 			{props.stats.map((stat, index) => (
 				<div key={index * 13} className='stats'>
 					{stat}
